e5b0295e26f13798bbdf156c1f51eb7cb8fea1d8
Bhushan <95331086+bhushantiturkar2001@users.noreply.github.com>
2025-03-30 11:22:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://bookstore-fromtend.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://bookstore-frontend-eta.vercel.app/'],
     credentials: true
 }))
 
